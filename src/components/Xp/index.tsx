57aba16470f18a6bf536a3215c9e9141f238f068
Natalia <natalia.araujo@yazo.com.br>
2022-12-24 00:07:55 -0300

    chore: type xp

--- src/components/Xp/index.tsx
+++ src/components/Xp/index.tsx
@@ -8,22 +8,28 @@ import {
   StackDivider,
   Text,
 } from "@chakra-ui/react";
+type XpProps = {
+  ocupation: string;
+  company: string;
+  description: string;
+};
 
-export function Xp() {
+export function Xp({
+  ocupation = " Desenvolvedora Front-end ReactJs",
+  company = "YAZO",
+  description = "Criação de funcionalidades para projetos do ramo de eventos, utlizando ReactJs, Typescript, Storybook, Material-UI, Styled components e Axios. Ferramentas gerenciais: Atlassian Jira, Scrum,Kanban, Github, AWS - S3, Cloud front",
+}: XpProps) {
   return (
     <Card bg="purple.900" w="100%" h="120px" borderRadius="6px">
       <CardBody>
         <Stack divider={<StackDivider />} spacing="4">
           <Box>
             <Heading size="xs" textTransform="uppercase" color="white">
-              <strong style={{ color: "#cf5192" }}> YAZO</strong> |
-              Desenvolvedora Front-end ReactJs
+              <strong style={{ color: "#cf5192" }}> {company}</strong> |
+              {ocupation}
             </Heading>
             <Text pt="2" fontSize="sm" color="white">
-              Criação de funcionalidades para projetos do ramo de eventos,
-              utlizando ReactJs, Typescript, Storybook, Material-UI, Styled
-              components e Axios. Ferramentas gerenciais: Atlassian Jira, Scrum,
-              Kanban, Github, AWS - S3, Cloud front.
+              {description}
             </Text>
           </Box>
         </Stack>
