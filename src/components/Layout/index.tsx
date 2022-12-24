2ad1ebe86d1a6b3b75e96e2e9397e78a3e32ba08
Natalia <natalia.araujo@yazo.com.br>
2022-12-23 23:12:20 -0300

    chore: types layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,7 @@
 import {
   Box,
-  Card,
   Divider,
   Flex,
-  HStack,
   Image,
   Tab,
   TabList,
@@ -13,17 +11,39 @@ import {
   Tag,
   Text,
 } from "@chakra-ui/react";
+import { ReactElement } from "react";
 
-export function Layout() {
+type LayoutProps = {
+  Name: string;
+  ocupation: string;
+  title: string;
+  nameRepo: string;
+  nameProject: string;
+  xp: string;
+  children: ReactElement;
+  children2: ReactElement;
+  children3: ReactElement;
+};
+export function Layout({
+  Name = "Natália Araújo",
+  ocupation = "Dev Frontend ReactJS",
+  title = "Sobre o meu trabalho",
+  nameRepo = "Repo Github",
+  nameProject = "Projetos",
+  xp = "Experiencias",
+  children,
+  children2,
+  children3,
+}: LayoutProps) {
   return (
     <Flex h="100vh" padding="1rem">
       <Flex w="357px" alignItems="center" h="100vh" flexDir="column" gap="4px">
         <Box marginBottom="32px">
           <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
           <Text fontSize="1.5rem" fontWeight="bold">
-            Natália Araújo
+            {Name}
           </Text>
-          <Text fontSize="1.1rem">Front-End developer</Text>
+          <Text fontSize="1.1rem">{ocupation}</Text>
         </Box>
         <Divider marginBottom="32px" width="320px" />
         <Tag
@@ -65,34 +85,34 @@ export function Layout() {
       </Flex>
       <Flex w="90%" h="100vh" marginLeft="42px" flexDir="column">
         <Text fontWeight="bold" fontSize="2.2rem">
-          Sobre o meu trabalho
+          {title}
         </Text>
         <Tabs variant="unstyled">
           <TabList gap="4px">
             <Tab w="150px" bg="purple.700" borderRadius="4px">
-              Repo Github
+              {nameRepo}
             </Tab>
             <Tab w="150px" bg="purple.700" borderRadius="4px">
-              Projetos
+              {nameProject}
             </Tab>
             <Tab isDisabled w="150px" bg="purple.700" borderRadius="4px">
-              Experiencias
+              {xp}
             </Tab>
           </TabList>
           <TabPanels>
             <TabPanel>
               <Flex bg="pink.900" maxWidth="895px" h="600px">
-                1
+                {children}
               </Flex>
             </TabPanel>
             <TabPanel>
               <Flex bg="pink.800" maxWidth="895px" h="600px">
-                2
+                {children2}
               </Flex>
             </TabPanel>
             <TabPanel>
               <Flex bg="pink.700" maxWidth="895px" h="600px">
-                3
+                {children3}
               </Flex>
             </TabPanel>
           </TabPanels>
